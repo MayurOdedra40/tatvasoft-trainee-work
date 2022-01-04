89da6ed89654b1b5663fc10928f61c41976ef752
Mayur Odedra <mayurodedra.40st@gmail.com>
2022-01-04 21:36:34 +0530

    Service provider login page code from psd
     this html page contain all service provider page which comes up when, user is logged into its account.
    
    Its also have all tabs working except service schedule, as its nice to have feature, it will be later made at integration time with popup and popover

--- HTML/js/helperland.js
+++ HTML/js/helperland.js
@@ -13,3 +13,48 @@ $("#accordion").on("hide.bs.collapse show.bs.collapse", e => {
       .find("img:last-child")
       .toggleClass("right rotated");
   });
+
+  $(document).ready(function () {
+
+    $('.dismiss, .overlay').on('click', function () {
+        // hide sidebar
+        $('.sidebar').removeClass('active');
+        // hide overlay
+        $('.overlay').removeClass('active');
+    });
+
+    $('.sidebarCollapse').on('click', function () {
+        // open sidebar
+        $('.sidebar').addClass('active');
+        // fade in the overlay
+        $('.overlay').addClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
+
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+        var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+            return new bootstrap.Popover(popoverTriggerEl)
+        })
+
+        $(function () {
+            $('[data-toggle="popover"]').popover({
+                html: true,
+                placement: "bottom",
+                title: function () {
+                    return $("#popover-title").html();
+                },
+                content: function () {
+                    return $('#popover-content').html();
+                },
+            });
+        });
+
+
+        jQuery(document).ready(function ($) {
+            $(".clickable-row").click(function () {
+                window.location = $(this).data("href");
+            });
+        });
