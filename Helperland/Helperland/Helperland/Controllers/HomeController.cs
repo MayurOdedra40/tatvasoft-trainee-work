8ec4659278940097ca6622729aa9b4c3473fa694
Mayur Odedra <mayurodedra.40st@gmail.com>
2022-01-28 19:06:06 +0530

    First Integration of Pages and DB
    All Public Pages are Integrated with Project

---
+++ Helperland/Helperland/Helperland/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Helperland.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helperland.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Faq()
+        {
+            return View();
+        }
+
+        public ViewResult Price()
+        {
+            return View();
+        } 
+
+        public ViewResult AboutUs()
+        {
+            return View();
+        }
+
+        public ViewResult ContactUs()
+        {
+            return View();
+        }
+
+        public ViewResult ServiceProvider()
+        {
+            return View();
+        }
+    }
+}
---
+++ Helperland/Helperland/Helperland/wwwroot/js/site.js
@@ -0,0 +1,65 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+$("#accordion").on("hide.bs.collapse show.bs.collapse", e => {
+    $(e.target)
+        .prev()
+        .find("img:last-child")
+        .toggleClass("right rotated");
+});
+
+
+$("#accordion2").on("hide.bs.collapse show.bs.collapse", e => {
+    $(e.target)
+        .prev()
+        .find("img:last-child")
+        .toggleClass("right rotated");
+});
+
+$(document).ready(function () {
+
+    $('.dismiss, .overlay').on('click', function () {
+        // hide sidebar
+        $('.sidebar').removeClass('active');
+        // hide overlay
+        $('.overlay').removeClass('active');
+    });
+
+    $('.sidebarCollapse').on('click', function () {
+        // open sidebar
+        $('.sidebar').addClass('active');
+        // fade in the overlay
+        $('.overlay').addClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
+
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
+
+$(function () {
+    $('[data-toggle="popover"]').popover({
+        html: true,
+        placement: "bottom",
+        title: function () {
+            return $("#popover-title").html();
+        },
+        content: function () {
+            return $('#popover-content').html();
+        },
+    });
+});
+
+
+jQuery(document).ready(function ($) {
+    $(".clickable-row").click(function () {
+        window.location = $(this).data("href");
+    });
+});
